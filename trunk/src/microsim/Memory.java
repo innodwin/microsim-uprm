cdfe8f1ab942f710f7dc4ac7229fe8a37fb4d304
damianesteves@gmail.com <damianesteves@gmail.com@6a824687-ab90-e4d1-9b58-2d4666888f99>
2011-10-10 00:42:26 +0000

    Rudimentary Memory class. getContent() and setContent()

--- trunk/src/microsim/Memory.java
+++ trunk/src/microsim/Memory.java
@@ -4,10 +4,23 @@
  */
 package microsim;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author damian
  */
 public class Memory {
+    private ArrayList <String> memory;
     
+    public String getContent(String address)
+    {
+        int dir = Integer.parseInt(address, 16);
+        return memory.get(dir);
+    }
+    
+    public void setContent(String address, String content)
+    {
+        memory.add(Integer.parseInt(address, 16), content);
+    }
 }
