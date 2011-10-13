071c3d6a83ff77b5cc86eb941cdab87688985d34
luis.rosario.berrios@gmail.com <luis.rosario.berrios@gmail.com@6a824687-ab90-e4d1-9b58-2d4666888f99>
2011-10-13 00:07:43 +0000

    Working of Registers Class. Added Tools class to write down common methods.

--- trunk/src/TestingClass.java
+++ trunk/src/TestingClass.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,5 +15,13 @@ public class TestingClass {
    
     public static void main(String[] args){
     System.out.println("Hello Damian!");
+    Random ram = new Random();
+    ArrayList<String> names= new ArrayList<String>();
+    names.add("Luis");
+    names.add("Daniel");
+    names.add("Damian");
+    
+    System.out.println(ram.nextInt(10));
+    
     }
 }
