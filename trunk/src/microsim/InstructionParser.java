642dd387a5f4d707b6691971f741ca2a13c3fd77
damianesteves@gmail.com <damianesteves@gmail.com@6a824687-ab90-e4d1-9b58-2d4666888f99>
2011-10-14 03:33:22 +0000

    Fleshed out InstructionParser class

--- trunk/src/microsim/InstructionParser.java
+++ trunk/src/microsim/InstructionParser.java
@@ -9,5 +9,40 @@ package microsim;
  * @author damian
  */
 public class InstructionParser {
+
     
+    /**
+     * Empty constructor. Class is only used for its methods
+     */
+    public InstructionParser() {
+        
+    }
+    
+    
+    /**
+     * Converts hexadecimal instruction String into a binary String
+     * @param instruction
+     * @return
+     */
+    public static String parse(String instruction){
+        //TODO: Check if instruction length = 4
+        int decimal = Integer.parseInt(instruction, 16); //Converts instruction to int
+        String binary = Integer.toBinaryString(decimal); //Converts int instruction to binary String
+        return binary;
+    }
+    
+    public static String opcode(String binaryInstruction){        
+        String opcode = binaryInstruction.substring(0, 4);
+        return opcode;
+    }
+    
+    public static String register(String binaryInstruction){
+        String register = binaryInstruction.substring(5, 7);
+        return register;
+    }
+    
+    public static String operand(String binaryInstruction){
+        String register = binaryInstruction.substring(8, 15);
+        return register;
+    }
 }
