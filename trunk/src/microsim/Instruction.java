5eee3591cfa2aa439c949875644655a0a9ae73a2
damianesteves@gmail.com <damianesteves@gmail.com@6a824687-ab90-e4d1-9b58-2d4666888f99>
2011-10-14 06:17:29 +0000

    Fleshed out InstructionParser class

---
+++ trunk/src/microsim/Instruction.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package microsim;
+
+
+/**
+ *
+ * @author damian
+ */
+public class Instruction {
+    
+    protected String hexInstruction;  
+    protected int intInstruction;
+    
+    public Instruction(String instruction) {
+        hexInstruction = instruction;
+        intInstruction = Integer.parseInt(hexInstruction, 16); //Converts instruction to int
+    }
+    
+    /**
+     * Get the value of hexInstruction
+     *
+     * @return the value of hexInstruction
+     */
+    public String getHexInstruction() {
+        return hexInstruction;
+    }
+    
+    public int getIntInstruction(){
+        return intInstruction;
+    }
+    
+    public String getBinInstruction() {
+        String binInstruction = Integer.toBinaryString(intInstruction);
+        int length = binInstruction.length();
+        if(length != 16)
+            for(int i = length; i < 16; i++)
+                binInstruction = "0" + binInstruction;
+        return binInstruction;
+    }
+
+
+}
--- trunk/src/microsim/InstructionParser.java
+++ trunk/src/microsim/InstructionParser.java
@@ -21,8 +21,8 @@ public class InstructionParser {
     
     /**
      * Converts hexadecimal instruction String into a binary String
-     * @param instruction
-     * @return
+     * @param instruction The instruction to convert
+     * @return The instruction in a binary String
      */
     public static String parse(String instruction){
         //TODO: Check if instruction length = 4
@@ -31,16 +31,31 @@ public class InstructionParser {
         return binary;
     }
     
+    /**
+     * Extracts the OpCode from a String that is already in binary format
+     * @param binaryInstruction
+     * @return The 5 opcode bits in a binary String
+     */
     public static String opcode(String binaryInstruction){        
         String opcode = binaryInstruction.substring(0, 4);
         return opcode;
     }
     
+    /**
+     * 
+     * @param binaryInstruction
+     * @return
+     */
     public static String register(String binaryInstruction){
         String register = binaryInstruction.substring(5, 7);
         return register;
     }
     
+    /**
+     * 
+     * @param binaryInstruction
+     * @return
+     */
     public static String operand(String binaryInstruction){
         String register = binaryInstruction.substring(8, 15);
         return register;
