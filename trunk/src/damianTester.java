4994aebab63194c96673c366e62fd09d965ef059
damianesteves@gmail.com <damianesteves@gmail.com@6a824687-ab90-e4d1-9b58-2d4666888f99>
2011-10-16 07:05:14 +0000

    Added more methods to CU

--- trunk/src/damianTester.java
+++ trunk/src/damianTester.java
@@ -1,7 +1,4 @@
 
-import microsim.Instruction;
-
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,14 +10,6 @@ import microsim.Instruction;
  */
 public class damianTester {
      public static void main(String[] args) {
-      Instruction test = new Instruction ("F4A6");   
-      Instruction test2 = new Instruction ("0000");
-      
-      System.out.println("Hex inst " + test.getHexInstruction());
-      System.out.println("Int inst " +test.getIntInstruction());
-      System.out.println("Bin inst " +test.getBinInstruction());
-      System.out.println("Opcode   " +test.getOpcode());
-      System.out.println("Register      "+test.getRegister());
-      System.out.println("Operand          "+test.getOperand());
+         
      }
 }
