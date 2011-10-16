8aae5536933d4b5f54c6073e0c91eef97543c6af
damianesteves@gmail.com <damianesteves@gmail.com@6a824687-ab90-e4d1-9b58-2d4666888f99>
2011-10-16 01:48:50 +0000

    Filled out a few more methods in ControlUnit

--- trunk/src/microsim/ControlUnit.java
+++ trunk/src/microsim/ControlUnit.java
@@ -4,10 +4,49 @@
  */
 package microsim;
 
+import java.io.File;
+import java.util.ArrayList;
+
 /**
- *
+ * Handles the Instructions received from the file
  * @author damian
  */
 public class ControlUnit {
+
+    private Memory memory;
+    private ArrayList<String> hexInstructions;    
+
+    public ControlUnit(File instructions) {        
+        /*
+         * Pseudocode tentativo:
+         * 
+         * Grab file, insert instructions in hex into a string.
+         * Make ArrayList of hex instructions. ArrayList size 128.
+         * check length of instructions is 4 characters before inserting. If <4 throw error.
+         * 
+         */
+    }
+    public void instructionsToMemory(){
+        int size = hexInstructions.size();
+        for(int i=0;i<size;i++){
+           String binInst = InstructionParser.parse(hexInstructions.get(i));
+           String address = Integer.toBinaryString(i);
+           memory.setContent(address, binInst);
+        }
+    }
+    
+    public void executeInstruction(String InstructionRegister){
+        String opcode = InstructionParser.opcode(InstructionRegister);
+        String register = InstructionParser.register(InstructionRegister);
+        String operand = InstructionParser.operand(InstructionRegister);
+        
+        int intopcode = Integer.parseInt(opcode, 2);
+        switch(intopcode){
+            case 0: ALU.and();//
+                    break;
+                
+        }
+        
+    }
     
 }
