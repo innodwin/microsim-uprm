0b701ff05f8c84d5273d95590d10f72b2af792c6
damianesteves@gmail.com <damianesteves@gmail.com@6a824687-ab90-e4d1-9b58-2d4666888f99>
2011-10-16 04:23:28 +0000

    Control Unit's executeInstruction() logically done. need to fix specific instruction calls.

--- trunk/src/microsim/ControlUnit.java
+++ trunk/src/microsim/ControlUnit.java
@@ -13,8 +13,9 @@ import java.util.ArrayList;
  */
 public class ControlUnit {
 
-    private Memory memory;
-    private ArrayList<String> hexInstructions;    
+    private Memory memory = new Memory();
+    private Registers registers = new Registers();
+    private ArrayList<String> hexInstructions = new ArrayList<String>(128);    
 
     public ControlUnit(File instructions) {        
         /*
@@ -38,15 +39,84 @@ public class ControlUnit {
     public void executeInstruction(String InstructionRegister){
         String opcode = InstructionParser.opcode(InstructionRegister);
         String register = InstructionParser.register(InstructionRegister);
+        String registerValue = registers.read(InstructionParser.register(InstructionRegister));
         String operand = InstructionParser.operand(InstructionRegister);
+        String accumulator = registers.getAccumulator();
+        String result = "";
         
         int intopcode = Integer.parseInt(opcode, 2);
         switch(intopcode){
-            case 0: ALU.and();//
-                    break;
-                
+            case 0: //AND
+                //ALU.and();
+                break;
+            case 1: //OR
+                //ALU.or();
+                break;
+            case 2: //XOR
+                //ALU.xor();
+                break;
+            case 3: //ADDC
+                //ALU.addc();
+                break;
+            case 4: //SUB
+                //ALU.sub();
+                break;
+            case 5: //MUL
+                //TODO, change multiply to parse these 2 8bit inputs into the 4 LSB.
+                result = ALU.multiply(accumulator, registerValue);
+                registers.setAccumulator(result);
+                break;
+            case 6: //NEG
+                //ALU.neg();
+                break;
+            case 7: //NOT
+                //ALU.not();
+                break;
+            case 8: //RLC
+                //ALU.rlc();
+                break;
+            case 9: //RRC
+                //ALU.rrc();
+                break;
+            case 10: //LDA rf
+                //ldarf();
+                break;
+            case 11: //STA rf
+                //starf();
+                break;
+            case 12: //LDA addr
+                //ldaddr();
+                break;
+            case 13: //STA addr
+                //staddr();
+                break;
+            case 14: //LDI Immediate
+                //ldi();
+                break;
+            case 16: //BRZ
+                //brz();
+                break;
+            case 17: //BRC
+                //brc();
+                break;
+            case 18: //BRN
+                //brn();
+                break;
+            case 19: //BRO
+                //bro();
+                break;
+            case 31: //STOP
+                //stop();
+                break;
+            case 24: //NOP
+                //nop();
+                break;
+            default: System.out.println("Invalid Opcode");
         }
         
     }
-    
+ 
+    public void ldarf(){
+        
+    }
 }
