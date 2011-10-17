89bd530784fb663e2cc90cc80145438c6f00cbe5
luis.rosario.berrios@gmail.com <luis.rosario.berrios@gmail.com@6a824687-ab90-e4d1-9b58-2d4666888f99>
2011-10-17 12:02:28 +0000

    Working of Registers Class. Added final variables in microsimapp class to refer to the registers..

--- trunk/src/microsim/Registers.java
+++ trunk/src/microsim/Registers.java
@@ -55,13 +55,13 @@ public class Registers {
         int addr = 0;
         int val = 0;
         try{
-            addr = Integer.parseInt(address, 2);
+            addr = Integer.parseInt(address, 2);//Verify if the address if formatted in binary.
            }catch(Exception e)
            { 
                return INVALIDFORMAT;
            }
         
-        if(addr > 7)
+        if(addr > 7) //Address of the general purpose register must be between 0-7.
             return INVALIDREGISTER;
         if(!t.isBinary(value))
             return this.INVALIDVALUE;
@@ -72,26 +72,27 @@ public class Registers {
         //If the value is not in an 8-bit format, this formats it.
         registers.set(addr, t.extendBinaryValue(8, value));
         
-        for(int i = 0; i < registers.size();i++)
-            System.out.println(registers.get(i));
+      /*  for(int i = 0; i < registers.size();i++)
+            System.out.println(registers.get(i));*/
         return 1;
         
     }
 
 //**************************************************************************************
     /**
-     * 
-     * @param addr
-     * @return 
+     * Reads a register from 0-7. Or from R1-R8.
+     * @param register
+     * @return A String of  binary characters representing the content of the specified
+     * register.
      */
 //**************************************************************************************
-    public String read(String address) {
+    public String read(String register) {
         int addr = 0;
         try{
-            addr = Integer.parseInt(address, 2);
+            addr = Integer.parseInt(register, 2);
            }catch(Exception e)
            { 
-               System.err.println("INVALID FORMAT ERROR Reading from register.. Value: " + address );
+               System.err.println("INVALID FORMAT ERROR Reading from register.. Value: " + register );
                return "";
            }
         
@@ -105,8 +106,13 @@ public class Registers {
     
     
 //**************************************************************************************
-//Sets the value of the Program Counter
+/**
+     * Sets the value of the Program Counter. Value must be in binary.
+     * @param value
+     * @return an error code if invalid. 1 if the operation was successful.
+     */
 //**************************************************************************************
+  
     public int setPC(String value)
     {
         if(!t.isBinary(value))
@@ -119,7 +125,10 @@ public class Registers {
     }
 
 //**************************************************************************************
-//Gets the value of the Program Counter
+    /**
+     * //Gets the value of the Program Counter
+     * @return  the Program Counter in its binary form.
+     */
 //**************************************************************************************
     public String getPC()
     {
@@ -127,7 +136,11 @@ public class Registers {
     }
 
 //**************************************************************************************
-//Sets the value of the Accumulator
+    /**
+     * Sets the value of the Accumulator. Value must be in binary.
+     * @param value
+     * @return an error code if invalid. 1 if the operation was successful.
+     */
 //**************************************************************************************
     public int setAccumulator(String value)
     {
@@ -141,7 +154,10 @@ public class Registers {
     }
 
 //**************************************************************************************
-//Gets the value of the Accumulator
+        /**
+         * Gets the value of the Accumulator
+         * @return The Accumulator value in its binary form.
+         */
 //************************************************************************************** 
     public String getAccumulator()
     {
